4374458a7690ff77ccbaea239e103a0644400b73
Joseph Mallari <joseph.mallari48@gmail.com>
2025-01-21 13:48:36 +0100

    make requests to fetchdata

---
+++ pages/api/fetchData.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { accessToken, location_id, limit } = req.query;
+
+    if (!accessToken) {
+      return res.status(400).json({ error: "Access token is required" });
+    }
+
+    if (!location_id) {
+      return res.status(400).json({ error: "Location ID is required" });
+    }
+
+    try {
+      const apiResponse = await axios.get("https://staging-api.arbolitics.com/data/getArboliticsDataset", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          location_id: location_id,
+          limit: 672,
+        },
+      });
+
+      const data = apiResponse.data;
+      console.log(data);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Error fetching dataset:", error);
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,18 +1,44 @@
 "use client";
 
+import React from "react";
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchDataset = async ({ queryKey }) => {
+  const [, accessToken, location_id, limit] = queryKey;
+  const response = await axios.get("/api/fetchData", {
+    params: {
+      accessToken,
+      location_id,
+      limit,
+    },
+  });
+  return response.data;
+};
 
 const data = () => {
   const searchParams = useSearchParams();
-  const userData = searchParams ? searchParams.get("userData") : null;
+  const accessToken = searchParams ? searchParams.get("userData") : null;
 
-  const parsedData = userData ? JSON.parse(userData) : null;
-  console.log(parsedData);
+  const parsedData = accessToken ? JSON.parse(accessToken) : null;
+  const {
+    data: dataset,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["dataset", parsedData, 10, 672], // Replace 10 and 1 with actual location_id and limit if needed
+    queryFn: fetchDataset,
+    enabled: !!parsedData, // Only run the query if parsedData is available
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching dataset: {error.message}</p>;
 
   return (
     <div>
       <h1>Data Component</h1>
-      {/* {parsedData && <pre>{JSON.stringify(parsedData, null, 2)}</pre>} */}
+      {dataset ? <pre>{JSON.stringify(dataset, null, 2)}</pre> : <p>No data available</p>}
     </div>
   );
 };
